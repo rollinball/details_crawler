44b1f13010982857a98e092d76d9d22f0c11c286
Samuel Kraus <sam@xtreams.io>
2018-05-09 21:41:00 +0300

    +

--- index.ts
+++ index.ts
@@ -12,20 +12,25 @@ const data: Array<any> = [
         address: 'Tel Aviv',
         tel: '0546546908'
     }
-] 
+]
 
 async function main ()
 {
     const search_api: customsearch_v1.Customsearch = new customsearch_v1.Customsearch({ }, new GoogleApis());
+    // here loop over each person details
     for (const entry of data) {
         const params: customsearch_v1.Params$Resource$Cse$List = {
             cx: '<search_engine_id>',
+            //this is the query
             q: `${entry.name}+${entry.address}`
         }
-        const list: AxiosResponse<customsearch_v1.Schema$Search> = await search_api.cse.list({}, {});
+        // here we perform the actual search query
+        const list: AxiosResponse<customsearch_v1.Schema$Search> = await search_api.cse.list(params, {});
         for (const item of list.data.items) {
+            // here we fetch the content at the url found by google
             const res: Response = await fetch(item.link);
             const content: string = await res.text();
+            // here we can play with the content, look for informations, check regexp, etc...
             if (content.search(entry.tel)) {
                 console.log('we have a match with the phone number');
             }
